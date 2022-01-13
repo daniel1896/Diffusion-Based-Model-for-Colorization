2884ccb1924ea87c50877272d3b8a8baeee40f5e
daniel1896 <daniel.henning@student.uni-luebeck.de>
2022-01-13 15:14:31 +0100

    Update dataloader.py

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,25 +1,21 @@
-import cv2
-import numpy as np
 import os
-import random
+import numpy as np
+from PIL import Image
 from torchvision.transforms import ToTensor
 from torch.utils.data import Dataset
 
-class gray_color_data(Dataset):
-    def __init__(self,path_color,path_gray):
-        super().__init__()
-        self.path_color = path_color
-        self.path_gray = path_gray
-        self.data_color = np.load(path_color)
-        self.data_gray = np.load(path_gray)
-    def __len__(self):
-        return len(self.data_color)
-    def __getitem__(self,idx):
-        image_gray =  self.data_gray[idx]
-        shape = (image_gray.shape[0],image_gray.shape[1],3)
-        image_color = np.zeros((224,224,3))
-        image_color[:,:,0] = image_gray
-        image_color[:,:,1:] = self.data_color[idx]
-        image_color = image_color.astype('uint8')
-        image_color = cv2.cvtColor(image_color,cv2.COLOR_LAB2RGB)
-        return(ToTensor()(image_gray),ToTensor()(image_color))
+class DatasetLoader(Dataset):
+  def __init__(self,dir_rgb,dir_tf):
+    super().__init__()
+    self.dir_rgb = dir_rgb
+    self.dir_tf = dir_tf
+    self.paths_rgb = [dir_rgb+f for f in listdir(dir_rgb) if isfile(join(dir_rgb, f))]
+    self.paths_tf = [dir_tf+f for f in listdir(dir_tf) if isfile(join(dir_tf, f))]
+  
+  def __len__(self):
+    return len(self.paths_rgb)
+  
+  def __getitem__(self,idx):
+    image_rgb = np.array(Image.open(self.paths_rgb[idx])).astype('uint8')
+    image_tf = np.array(Image.open(self.paths_tf[idx])).astype('uint8')
+    return(ToTensor()(image_tf),ToTensor()(image_rgb))
